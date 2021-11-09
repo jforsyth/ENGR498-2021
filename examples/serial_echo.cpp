dfc037780d6bae57cb8d9726415fa957a74e7641
Jason Forsyth <forsy2jb@jmu.edu>
2021-11-08 22:41:33 -0500

    Add Serial Echo example

---
+++ examples/serial_echo.cpp
@@ -0,0 +1,22 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  USBDevice.attach();
+  
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+  while(Serial.available()>0)
+  {
+	  char c = Serial.read();
+	  Serial.print(c);
+  }
+}
