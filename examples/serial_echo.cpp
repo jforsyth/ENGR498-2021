5e89a011fa1cc94790a77e308415fc8656877c39
Jason Forsyth <forsy2jb@jmu.edu>
2021-11-08 22:52:14 -0500

    Update to Blink light

--- examples/serial_echo.cpp
+++ examples/serial_echo.cpp
@@ -6,17 +6,25 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   
+  digitalWrite(LED_BUILTIN,LOW);
+  
   USBDevice.attach();
   
   Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
+boolean led_state = false;
 void loop() {
   
   while(Serial.available()>0)
   {
 	  char c = Serial.read();
 	  Serial.print(c);
+	  
+	  digitalWrite(LED_BUILTIN,led_state);
+	  led_state=!led_state;
   }
+  
+  delay(10);
 }
