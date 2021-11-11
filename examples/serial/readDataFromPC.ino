abfa26e0906d022004f3203c4799b71b44fff446
Jason Forsyth <forsy2jb@jmu.edu>
2021-11-11 10:18:42 -0500

    Adruino example to read data from Serial port as CSV

---
+++ examples/serial/readDataFromPC.ino
@@ -0,0 +1,34 @@
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{
+  while (Serial.available())
+  {
+
+    //read in a line from the Serial port
+    String str = Serial.readString();
+
+    //trim the string to remove trailing white space
+    str.trim();
+
+    //locate all the stupid commas (assuming three values)
+    int firstComma = str.indexOf(",");
+    int secondComma = str.indexOf(",", firstComma + 1);
+
+    String x_str = str.substring(0, firstComma);
+    String y_str = str.substring(firstComma + 1, secondComma);
+    String z_str = str.substring(secondComma + 1, str.length());
+
+
+    int x_accel = x_str.toInt();
+    int y_accel = y_str.toInt();
+    int z_accel = z_str.toInt();
+    
+  }
+
+}
